16531cb092c6eead1ee0fad692d1976aca0dd32d
titan <titan@titan-pc>
2014-10-10 16:06:44 -0500

    Se recfactorizo el controlador y se agrego el parametro de la ciudad por defecto en config

--- src/Cupon/OfertaBundle/Controller/DefaultController.php
+++ src/Cupon/OfertaBundle/Controller/DefaultController.php
@@ -3,18 +3,26 @@
 namespace Cupon\OfertaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Symfony\Component\HttpFoundation\RedirectResponse;
 class DefaultController extends Controller
 {
     public function ayudaAction()
     {
         return $this->render('OfertaBundle:Default:ayuda.html.twig');
     }
-    public function portadaAction() {
+    public function portadaAction($ciudad = null) {
+        
+        if($ciudad == null){
+           $ciudad = $this->container->getParameter('cupon.ciudad_por_defecto'); 
+           return new RedirectResponse(
+                   $this->generateUrl('portada', array('ciudad' => $ciudad))
+                   );
+        }
+        
         $em = $this->getDoctrine()->getEntityManager();
         
         $oferta = $em->getRepository('OfertaBundle:Oferta')->findOneBy(array(
-                'ciudad' => 202
+                'ciudad' => $this->container->getParameter('cupon.ciudad_por_defecto')    
             ));
         return $this->render('OfertaBundle:Default:portada.html.twig',array('oferta' => $oferta));
     }
